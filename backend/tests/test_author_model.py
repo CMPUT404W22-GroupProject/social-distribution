dda94a1854475d6636b3f95ae38b8bdfd8d4d99f
tpbach <tpbach@ualberta.ca>
2022-02-20 10:53:08 -0700

    Change model test to only use model itself

--- backend/tests/test_author_model.py
+++ backend/tests/test_author_model.py
@@ -1,4 +1,3 @@
-from unittest import TestResult
 from django.test import TestCase
 from author.models import Author
 from author.serializers import AuthorsSerializer
@@ -9,25 +8,22 @@ class AuthorModelTest(TestCase):
     def setUp(self):
         
         self.test_dict = {
+             "host" : 'http://127.0.0.1:8000/',
              "displayName" : "testAuthor",
              "github" : "https://github.com/CMPUT404W22-GroupProject/social-distribution",
             }
 
-        # Create an Author via Serializer
-        serializer = AuthorsSerializer(data = self.test_dict)
-        if serializer.is_valid():
-            serializer.save()
-            author_id = serializer.data["id"]
-            self.author = Author.objects.get(pk = author_id)
-            self.host = 'http://127.0.0.1:8000/'
-            self.url = 'http://127.0.0.1:8000/{0}'.format(author_id)
+        self.author = Author.objects.create(**self.test_dict)
+        
+        self.url = "http://127.0.0.1:8000/{0}".format(self.author.id)
+        self.author.url = self.url
 
     def testAuthorModel(self):
         """Test all Author fields"""
         
         self.assertEqual(self.author.type, "author")
         self.assertEqual(self.author.url, self.url)
-        self.assertEqual(self.author.host, self.host)
+        self.assertEqual(self.author.host, self.test_dict["host"])
         self.assertEqual(self.author.displayName, self.test_dict["displayName"])
         self.assertEqual(self.author.github, self.test_dict["github"])
 
