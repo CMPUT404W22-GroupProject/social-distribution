c3d896dfb7bd518b4c0988c5831c0aead30004be
v6k <user@Users-MacBook-Air.local>
2022-02-22 16:15:37 -0700

    Fixing branch

--- backend/author/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.AuthorList.as_view()),
-    path('<int:author_id>/', views.AuthorDetails.as_view()),
-]
