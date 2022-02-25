43a3850cffb50506a2976d4d9ecff1758615abcc
Gurjog Singh <gurjog@ualberta.ca>
2022-02-25 02:40:08 -0700

    added post, comments, createPost, feed components
    functionality and template for most features, just need to figure out way to connect to backend to see  it in fruition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import Home from './pages/Home';
-import Login from './pages/Login';
+import Home from './pages/home';
+import Login from './pages/login';
 import Register from './pages/Register';
 import NavBar from './components/Navbar';
 
---
+++ frontend/src/components/feed/Feed.jsx
@@ -0,0 +1,23 @@
+import "./feed.css"
+import CreatePost from '../createPost/CreatePost'
+import Post from '../Post'
+
+function Feed(){
+
+    return (
+
+        <div>
+        <CreatePost/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+
+        </div>
+    )
+
+}
+
+export default Feed;
