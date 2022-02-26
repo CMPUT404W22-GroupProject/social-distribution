0f89b849672f3b2db1b01f9f691dffe83a52dfcd
v6k <yearoo@ualberta.ca>
2022-02-26 16:38:50 -0700

    Removed LIKES tests

--- backend/like/views.py
+++ backend/like/views.py
@@ -14,7 +14,6 @@ class LikeList(APIView):
         all_likes = Like.objects.all()
         serializer = LikeSerializer(all_likes, many = True)
         for each_object in serializer.data:
-            print(serializer.data)
             author = Author.objects.get(pk=each_object['author']).toString()
             author['id']=each_object['author']
             each_object['author']=author
