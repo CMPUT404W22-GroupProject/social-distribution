4bbb308c5b95bcf79ef09ac196fbe444743f4cf2
v6k <yearoo@ualberta.ca>
2022-02-27 22:00:37 -0700

    Fixed likes POST

--- backend/like/models.py
+++ backend/like/models.py
@@ -6,8 +6,8 @@ class Like(models.Model):
         context = models.TextField(default="https://www.w3.org/ns/activitystreams")
         summary = models.TextField(blank=True)
         type = models.TextField(default="Like")
-        author = models.ForeignKey(Author, on_delete=models.CASCADE)
-        object = models.ForeignKey(Post, on_delete=models.CASCADE)
+        author = models.ForeignKey(Author, on_delete=models.CASCADE, default=1)
+        object = models.ForeignKey(Post, on_delete=models.CASCADE,default="8e1d40fa-ca45-496c-a409-fcc39d5026bf")
         # object for comments
 
 '''
--- backend/like/views.py
+++ backend/like/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from author.models import Author
+from post.models import Post
 from like.models import Like
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -21,13 +22,17 @@ class LikeList(APIView):
         return Response(serializer.data, status = 200)
 
     # Add a like object
-    def post(self, request):
+    def post(self, request, author_id, post_id):
         # Mutable copy
         request_data = request.data.copy()
         serializer = LikeSerializer(data = request_data)
-        author = Author.objects.get(pk=serializer.initial_data['author']).displayName
+        author1 = Author.objects.get(pk=author_id)
+        post1 = Post.objects.get(pk=post_id)
+        
         if serializer.is_valid():
-            serializer.save(summary = author + " likes your post")
+            serializer.save(summary = author1.displayName + " likes your post")
+            serializer.save(author = author1)
+            serializer.save(object = post1)
             return Response(serializer.data, status = 201)
         else:
             return Response(serializer.errors, status = 400)
