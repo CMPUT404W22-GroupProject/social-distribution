95c2f68020566310a5ed428d2a1cdf67e1ce59d5
v6k <yearoo@ualberta.ca>
2022-02-28 23:51:37 -0700

    Created new function for erro checking

--- backend/like/views.py
+++ backend/like/views.py
@@ -13,7 +13,7 @@ from like.serializers import LikeSerializer
 # Create your views here.
 class LikeList(APIView):
     # Get all Likes, for all authors
-    def get(self, request, author_id, post_id, comment_id=""): 
+    def checkErors(self, author_id, post_id, comment_id):
         try:
             Author.objects.get(pk=author_id)
         except Author.DoesNotExist:
@@ -28,6 +28,8 @@ class LikeList(APIView):
         except Comment.DoesNotExist:
             return HttpResponse("Comment not found", status=404)
 
+    def get(self, request, author_id, post_id, comment_id=""): 
+        self.checkErors(author_id,post_id, comment_id)
         #Creating the links to objects
         http_host = request.META.get('HTTP_HOST')
         if http_host[0]!="h":
@@ -41,6 +43,7 @@ class LikeList(APIView):
                 post_object = post_object + "/" + each_detail
 
         all_likes = Like.objects.all()
+        print(all_likes)
         serializer = LikeSerializer(all_likes, many = True)
         result = []
         if comment_id=="":
@@ -48,8 +51,6 @@ class LikeList(APIView):
             for each_object in serializer.data:
                 if not each_object['object']:
                     continue
-                # if each_object['summary'].split(" ")[-1].lower() == "comment":
-                #     continue
                 level = {}
                 author = Author.objects.get(pk=each_object['author']).toString()
                 for each_item in each_object:
@@ -69,8 +70,7 @@ class LikeList(APIView):
             for each_object in serializer.data:
                 if not each_object['object1']:
                     continue
-                # if each_object['summary'].split(" ")[-1].lower() == "post":
-                #     continue
+                print("Its a comment")
                 level = {}
                 author = Author.objects.get(pk=each_object['author']).toString()
                 for each_item in each_object:
@@ -89,51 +89,30 @@ class LikeList(APIView):
 
     # Add a like object FOR POSTS
     def post(self, request, author_id, post_id, comment_id=""):
-        try:
-            Author.objects.get(pk=author_id)
-        except Author.DoesNotExist:
-            return HttpResponse("Author not found", status=404)
-        try:
-            Post.objects.get(pk=post_id)
-        except Post.DoesNotExist:
-            return HttpResponse("Post not found", status=404)
-        try:
-            if comment_id!="":
-                Comment.objects.get(pk=comment_id)
-        except Comment.DoesNotExist:
-            return HttpResponse("Comment not found", status=404)
-
+        self.checkErors(author_id,post_id, comment_id)
         # Mutable copy
-        if comment_id=="":
-            request_data = request.data.copy()
-            serializer = LikeSerializer(data = request_data)
-            author1 = Author.objects.get(pk=serializer.initial_data['author'])
-            post1 = Post.objects.get(pk=post_id)
-            if serializer.is_valid():
-                serializer.save(summary = author1.displayName + " likes your post")
-                serializer.save(author = author1)
-                serializer.save(object = post1)
-                return Response(serializer.data, status = 201)
-            else:
-                return Response(serializer.errors, status = 400)
-        else:
-            # Mutable copy
-            request_data = request.data.copy()
-            serializer = LikeSerializer(data = request_data)
-            author1 = Author.objects.get(pk=serializer.initial_data['author'])
+        request_data = request.data.copy()
+        serializer = LikeSerializer(data = request_data)
+        author1 = Author.objects.get(pk=serializer.initial_data['author'])
+        if comment_id !="":
             comment1 = Comment.objects.get(pk=comment_id)
-            if serializer.is_valid():
-                serializer.save(summary = author1.displayName + " likes your comment")
-                serializer.save(author = author1)
-                serializer.save(object1 = comment1)
-                return Response(serializer.data, status = 201)
+        else:
+            post1 = Post.objects.get(pk=post_id)
+        if serializer.is_valid():
+            if comment_id=="":
+                serializer.save(summary = author1.displayName + " likes your post")
+                serializer.save(object = post1)
             else:
-                return Response(serializer.errors, status = 400)
+                serializer.save(summary = author1.displayName + " likes your comment")
+                serializer.save(object1 = comment1)
+            serializer.save(author = author1)
+            return Response(serializer.data, status = 201)
+        else:
+            return Response(serializer.errors, status = 400)
 
 class LikedDetails(APIView):
-    # We require a author_id to be passed with the request (in the url) to get a like object
-    # Get the likes of a specific author
-    def get(self, request, author_id, post_id, like_id, comment_id=""):
+    # We require a author_id, post_id, and/or comment_id to be passed with the request (in the url) to get a like object
+    def checkErrors(self, author_id, post_id, like_id, comment_id=""):
         try:
             Author.objects.get(pk=author_id)
         except Author.DoesNotExist:
@@ -152,95 +131,51 @@ class LikedDetails(APIView):
         except Like.DoesNotExist:
             return HttpResponse("Like not found", status=404)
 
+    def get(self, request, author_id, post_id, like_id, comment_id=""):
+        self.checkErrors(author_id, post_id, like_id, comment_id)
         http_host = request.META.get('HTTP_HOST')
         if http_host[0]!="h":
             http_host = "http://"+http_host
         path_info = request.META.get('PATH_INFO').split("/")
         post_object = http_host
-        print(path_info)
         for each_detail in path_info:
             if each_detail == "likes":
                 break
             if each_detail != "":
                 post_object = post_object + "/" + each_detail
         # INCLUDE PERMISSION CHECKS BEFORE DOING THIS
-        if comment_id == "":
-            #FOR POSTS
-            try:
-                like = Like.objects.get(pk=like_id)
-                serializer = LikeSerializer(like)
-                result = {}
-                author = Author.objects.get(pk=author_id).toString()
-                for each_object in serializer.data:
-                    if each_object =="context":
-                        result["@context"] = serializer.data["context"]
-                    elif each_object == "object":
-                        result[each_object] = post_object
-                    # elif each_object=="id":
-                    #     result[each_object] = http_host+path_info
-                    elif each_object != "author" and each_object!="object1" and each_object!="id":
-                        result[each_object] = serializer.data[each_object]
-                    elif each_object == "author":
-                        result[each_object]= author
-                return Response(result, status = 200)
-            except Like.DoesNotExist:
-                return HttpResponse("Like not found.", status = 401)
-        else:
-            #FOR COMMENTS
-            try:
-                like = Like.objects.get(pk=like_id)
-                serializer = LikeSerializer(like)
-                result = {}
-                author = Author.objects.get(pk=author_id).toString()
-                for each_object in serializer.data:
-                    if each_object =="context":
-                        result["@context"] = serializer.data[each_object]
-                    elif each_object == "object1":
-                        result["object"]= post_object
-                    # elif each_object=="id":
-                    #     result[each_object] = http_host+path_info
-                    elif each_object != "author" and each_object!="object" and each_object!="id":
-                        result[each_object] = serializer.data[each_object]
-                    elif each_object == "author":
-                        result[each_object]= author
-                return Response(result, status = 200)
-            except Like.DoesNotExist:
-                return HttpResponse("Like not found.", status = 401)
+        like = Like.objects.get(pk=like_id)
+        serializer = LikeSerializer(like)
+        result = {}
+        author = Author.objects.get(pk=author_id).toString()
+        try:
+            for each_object in serializer.data:
+                if each_object =="context":
+                    result["@context"] = serializer.data[each_object]
+                elif each_object == "object1" and comment_id != "":
+                    result["object"]= post_object
+                elif each_object == "object" and comment_id=="":
+                    result[each_object] = post_object
+                elif each_object != "author" and each_object!="object" and each_object!="id":
+                    result[each_object] = serializer.data[each_object]
+                elif each_object == "author":
+                    result[each_object]= author
+            return Response(result, status = 200)
+        except Like.DoesNotExist:
+            return HttpResponse("Like not found.", status = 401)
 
 
     #Unlike a post or comment
     def delete(self, request, author_id, post_id, like_id, comment_id=""):
-        try:
-            Author.objects.get(pk=author_id)
-        except Author.DoesNotExist:
-            return HttpResponse("Author not found", status=404)
-        try:
-            Post.objects.get(pk=post_id)
-        except Post.DoesNotExist:
-            return HttpResponse("Post not found", status=404)
-        try:
-            if comment_id!="":
-                Comment.objects.get(pk=comment_id)
-        except Comment.DoesNotExist:
-            return HttpResponse("Comment not found", status=404)
-        try:
-            Like.objects.get(pk=like_id)
-        except Like.DoesNotExist:
-            return HttpResponse("Like not found", status=404)
-
+        self.checkErrors(author_id, post_id, like_id, comment_id)
         # INCLUDE PERMISSION CHECKS BEFORE DOING THIS
-        if comment_id=="":
-            #FOR POSTS
-            try:
-                like = Like.objects.get(pk=like_id)
-                like.delete()
+        try:
+            like = Like.objects.get(pk=like_id)
+            like.delete()
+            if comment_id=="":
                 return HttpResponse("Successfully unliked the post.", status=201)
-            except  Like.DoesNotExist:
-                return HttpResponse("Like object not found.", status=401) 
-        else:
-            try:
-                like = Like.objects.get(pk=like_id)
-                like.delete()
+            else:
                 return HttpResponse("Successfully unliked the comment.", status=201)
-            except  Like.DoesNotExist:
-                return HttpResponse("Like object not found.", status=401) 
+        except  Like.DoesNotExist:
+            return HttpResponse("Like object not found.", status=401) 
+
