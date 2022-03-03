05765e656b092445d56a3dc44405d4a56274fa26
Nick Anderson <47461646+nick98anderson@users.noreply.github.com>
2022-03-02 19:40:52 -0700

    user context updated

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,20 +1,41 @@
-import React, {useContext, useEffect, useState} from 'react' ;
-import {useSelector} from 'react-redux';
-import  {selectUser} from '../../features/userSlice';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react' 
+import Feed from '../components/feed/Feed'
+import UserContext from '../context/userContext';
+import axios from "axios"
+import Post from '../components/Post';
 
-function Profile(){
+
+function Home(){
+
+    const {id, setId} = useContext(UserContext);
+    const [authorData, setAuthorData] = useState('')
+
+    useEffect(() => {
+        fetchAuthor()
+    }, [null])
 
 
-    useEffect(async () => {
-    }, [])
+    async function fetchAuthor() {
+        const res = await axios.get('authors/2');
+        setAuthorData(res.data)
+    }
+
+    async function fetchPosts(){
+        const res = await axios.get('authors/2/posts')
+        console.log(res.data)
+    }
 
     return (
         <div>
-           <h1></h1>
-           <h1></h1>
+            <h1>{authorData.displayName}</h1>
+            <h1>{authorData.host}</h1>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+
         </div>
     )
 }
 
-export default Profile;
+export default Home;
