358b519fccacfb6afdf969dd38869b40fc885c57
moenuma <moenumasawa@gmail.com>
2022-03-04 07:06:03 +0900

    Add image field to Post

--- backend/post/models.py
+++ backend/post/models.py
@@ -7,16 +7,16 @@ from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    type = models.TextField(default="post")
+    type = models.CharField(default="post", max_length=255)
     title = models.CharField(max_length=255)
     source = models.URLField(blank=True) #TODO we are not gonna implement share feature for part 1 so this will be the same as id
     origin = models.URLField(blank=True) #TODO same as id
     description = models.TextField()
     contentType = models.CharField(max_length=255)
-    content = models.TextField() #TODO does backend handle this?
+    content = models.TextField()
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="post_author", on_delete=models.CASCADE) # when Author is deleted, Post will be deleted
     categories = models.TextField()
-    # count = models.PositiveBigIntegerField()
     published = models.DateTimeField(default=timezone.now)
     visibility = models.CharField(max_length=255)
     unlisted = models.BooleanField()
+    image = models.TextField(blank=True)
