f5372e2d5b78c7a01ce191c1423b78e21c790c08
v6k <yearoo@ualberta.ca>
2022-03-17 21:24:02 -0600

    Added inbox for posts and comments

--- backend/inbox/views.py
+++ backend/inbox/views.py
@@ -2,7 +2,9 @@ from re import search
 import re
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, request
+from like.models import Like
 from inbox.models import Inbox
+from comment.models import Comment
 from like.serializers import LikeSerializer
 from post.models import Post
 from author.models import Author
@@ -45,9 +47,38 @@ class InboxList(ListCreateAPIView):
         serializer =  InboxSerializer(queryset, many=True, context={'listRequest':request})
         return Response(serializer.data, status=200)
 
-      # Add a like object FOR POSTS
     def post(self, request, author_id):
-        return HttpResponse("Sent to inbox", status=201) 
+        request_data = request.data.copy()
+
+        # An inbox object is created whenever a post, like, comment, follow is sent. 
+        # This object refers to the original item sent through their id.
+
+        try:
+            #If the request type is a post
+            if request_data["type"].lower()=="post":
+                post_id = request_data["id"]
+                new_post = Post.objects.get(uuid = post_id)
+                Inbox.create_object_from_post(new_post)
+
+            #If the request type is a comment
+            elif request_data["type"].lower()=="comment":
+                comment_id = request_data["id"]
+                new_comment = Comment.objects.get(uuid = comment_id)
+                Inbox.create_object_from_comment(new_comment)
+
+            #If the request type is a like
+            elif request_data["type"].lower()=="like":
+                like_id = request_data["id"]
+                new_like = Like.objects.get(id = like_id)
+                Inbox.create_object_from_like(new_like)
+
+            #If the request type is a follow
+            # else:
+            
+            return HttpResponse("Sent to inbox", status=201) 
+        except:
+            return HttpResponse("Error", status=400) 
+
 
     #Clear inbox
     def delete(self, request, author_id):
@@ -58,3 +89,12 @@ class InboxList(ListCreateAPIView):
             return HttpResponse("Successfully cleared inbox.", status=201)
         except  Inbox.DoesNotExist:
             return HttpResponse("No inbox found.", status=401) 
+
+
+
+'''
+{
+    "type": "comment",
+"id":"ce968aa5-0a10-4f98-8206-3df0e5c24a58"
+}
+'''
