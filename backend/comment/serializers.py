a95b9a744584b194841cdcf04465304ad951c0d2
moenuma <moenumasawa@gmail.com>
2022-03-22 17:28:39 +0900

    Retrieve Author for Comment

--- backend/comment/serializers.py
+++ backend/comment/serializers.py
@@ -8,29 +8,21 @@ import uuid
 from author.models import Author
 
 class CommentSerializer(ModelSerializer):
-    # author = AuthorsSerializer(many=False, read_only=True)
     id = SerializerMethodField()
-    author = SerializerMethodField()
 
     class Meta:
         model = Comment
         fields = ('type', 'author', 'comment', 'contentType', 'published', 'id')
 
     def get_id(self, comment):
-        try:
-            request = self.context.get('request')
-            return request.build_absolute_uri() + str(comment.uuid)
-        except:
-            "none"
-    
-    def get_author(self, comment):
         request = self.context.get('request')
-        request_uuid = uuid.UUID(str(request.user))
-        author = Author.objects.get(pk=request_uuid)
-        serializer = AuthorsSerializer(author, context={'request':request})
-        return serializer.data
+        url_no_id = request.build_absolute_uri().split('/comments/')[0]
+        return url_no_id + '/comments/' + str(comment.uuid)
 
     def create(self, validated_data):
         validated_data['post'] = self.context.get('post')
         new_comment = Comment.objects.create(**validated_data)
         return new_comment
+
+class CommentSerializerGet(CommentSerializer):
+    author = AuthorsSerializer(many=False, read_only=True)
