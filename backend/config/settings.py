699714262f137745fc11b7678c77d96e26bb291b
moenuma <moenumasawa@gmail.com>
2022-03-23 04:58:49 +0900

    Authentication disabled

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,17 +78,17 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAdminUser',
+    # ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
   
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     'rest_framework.authentication.TokenAuthentication',
+    #     'rest_framework.authentication.BasicAuthentication',
+    # ),
 }
 
 ROOT_URLCONF = 'config.urls'
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('authors/', include('author.urls')),
     path('admin/', admin.site.urls),
     # path('like/', include('like.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    # path('api-auth/', include('rest_framework.urls')),
 ]
