111a92fd95b00fadd0aa3aaf1b4f7e78523e7285
moenuma <moenumasawa@gmail.com>
2022-03-25 20:52:13 +0900

    Refactor follower model

--- backend/follower/serializers.py
+++ backend/follower/serializers.py
@@ -1,16 +1,33 @@
+from jinja2 import FileSystemLoader
 from follower.models import FollowRequest
 from follower.models import Follower
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from author.serializers import AuthorsSerializer
 from author.models import Author
+import requests
 
 class FollowerSerializer(ModelSerializer):
-    items = AuthorsSerializer(many=True)
 
     class Meta:
         model = Follower
-        fields = ("type", "items")
-    
+        fields = ("type", "author", "object")
+
+class FollowerSerializerGet(FollowerSerializer):
+    object = SerializerMethodField()
+    author = AuthorsSerializer(many=False, read_only=True)
+
+    def get_object(self, follower):
+        response = requests.get(follower.object)
+        if response.status_code == 404:
+            follower_object =Follower.objects.get(pk=follower.id)
+            follower_object.delete()
+            return 
+        elif response.status_code != 200:
+            return follower.object
+        else:
+            return response.json()
+
+
 class FollowRequestSerializer(ModelSerializer):
     # actor = AuthorsSerializer(many=False, read_only=True)
     # object = AuthorsSerializer(many=False, read_only=True)
--- backend/follower/views.py
+++ backend/follower/views.py
@@ -5,67 +5,124 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from author.serializers import AuthorsSerializer
-from follower.serializers import FollowerSerializer
+from follower.serializers import FollowerSerializer, FollowerSerializerGet
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.mixins import LoginRequiredMixin
 import os
 from urllib.parse import urlparse
 import requests
+from node.authentication import BasicAuthentication
 
 # Create your views here.
 class FollowerList(APIView):
+    basic_auth = BasicAuthentication()
 
     def get(self, request, author_id):
+        response = self.basic_auth.remote_request(request)
+        if response:
+            return response
+
         try:
-            follower = Follower.objects.get(author=author_id)
-            serializer = FollowerSerializer(follower, context={'request':request})
-            return Response(serializer.data, status=200)
+            follower = Follower.objects.filter(author=author_id)
+            if not follower:
+                return Response("Follower not found", status=404)
+
+            serializer = FollowerSerializerGet(follower, many=True, context={'request':request})
+            items = []
+            for each_object in serializer.data:
+                items.append(each_object['object'])
+            
+            result = {}
+            result['type'] = "followers"
+            result['items'] =items
+
+            return Response(result, status=200)
 
         except Follower.DoesNotExist:
             return Response("Follower not found", status=404)
 
 class FollowerDetails(APIView, LoginRequiredMixin):
+    basic_auth = BasicAuthentication()
 
     def get(self, request, author_id, foreign_author_id):
-        try:
-            follower = Follower.objects.get(author=author_id)
-            item = follower.items.get(pk=foreign_author_id)
-            serializer = AuthorsSerializer(item, context={'request':request})
-            return Response(serializer.data, status=200)
-        except:
-            return Response("Follower not found", status=404)
+        response = self.basic_auth.remote_request(request)
+        if response:
+            return response
+
+        items = Follower.objects.filter(author=author_id)
+
+        for item in items:
+            follower_id = item.object.split('authors/')[1].split('/')[0]
+            if follower_id == str(foreign_author_id):
+                serializer = FollowerSerializerGet(item, context={'request':request})
+                return Response(serializer.data, status=200)
+
+        return Response("Follow not found", status=404)
 
     
     def put(self, request, author_id, foreign_author_id):
+        response = self.basic_auth.local_request(request)
+        if response:
+            return response
+
         if author_id == foreign_author_id:
             return Response("You cannot follow yourself", status=400)
+
+        request_data = request.data.copy()
+
+        if request_data['type'].lower() != "follow":
+            return Response("Incorrect object passed", status=400)
+
         try:
-            author = Author.objects.get(pk=author_id)
-            new_follower = Author.objects.get(pk=foreign_author_id)
+            author_id = request_data['object']['id']
+            author = Author.objects.get(id=author_id)
+            request_data['author'] = author.uuid
+            request_data.pop('object', None)
+
         except Author.DoesNotExist:
             return Response("Author does not exist", status=404)
         try:
-            instance = Follower.objects.get(author=author_id)
-            instance.items.add(new_follower)
-        except Follower.DoesNotExist:
-            instance = Follower.objects.create(author=author, items=[])
-            instance.items.set(new_follower)
+            request_data['type'] = "follower"
+            request_data.pop('summary', None)
+
+            follower_id = request_data['actor']['id']
+            follower_uuid = follower_id.split('authors/')[1].split('/')[0]
+
+            if follower_uuid != str(foreign_author_id):
+                return Response("Bad request", status=400)
+
+            follower_response = requests.get(follower_id)
+            if follower_response.status_code == 404:
+                return Response("Follower not found", status=404)
+
+            request_data['object'] = follower_id
+            request_data.pop('actor', None)
         
-        serializer = FollowerSerializer(instance, context={'request':request})
-        return Response(serializer.data, status=201)
+            serializer = FollowerSerializer(data=request_data, context={'request':request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=201)
+            else:
+                return Response(serializer.errors, status = 400)
+        
+        except Exception as e:
+            return Response("Follower action failed", status=400)
 
 
 
     def delete(self, request, author_id, foreign_author_id):
+        response = self.basic_auth.local_request(request)
+        if response:
+            return response
+            
         if author_id == foreign_author_id:
             return Response("You cannot perform following actions to yourself", status=400)
-        try:
-            instance = Follower.objects.get(author=author_id)
-        except Follower.DoesNotExist:
-            return Response("There's no one to unfollow", status=400)
-        try:
-            instance.items.remove(foreign_author_id)
-            instance.save()
-            return Response("Unfollow successfully", status=204)
-        except Author.DoesNotExist:
-            return Response("You cannot unfollow", status=401)
+        
+        items = Follower.objects.filter(author=author_id)
+        for item in items:
+            follower_id = item.object.split('authors/')[1].split('/')[0]
+            if follower_id == str(foreign_author_id):
+                item.delete()
+                return Response("Unfollow successfully", status=204)
+
+        return Response("You cannot unfollow", status=401)
