7e93ba806d4ede037e311744dfc79c98f8f61945
moenuma <moenumasawa@gmail.com>
2022-03-25 20:54:53 +0900

    Fix bugs with inbox (runtimeerror)

--- backend/inbox/serializers.py
+++ backend/inbox/serializers.py
@@ -10,6 +10,8 @@ import requests
 from django.urls import path
 from post.serializers import PostSerializerGet
 from comment.serializers import CommentSerializerGet
+from like.serializers import LikeSerializerGet
+from post.serializers import PostSerializerGet
 from urllib.parse import urlparse
 
 class InboxSerializer(ModelSerializer):
@@ -21,30 +23,28 @@ class InboxSerializer(ModelSerializer):
         fields = ('type', 'author', 'items')
 
     def make_request(self, url):
-        try:
-            response = requests.get(url).json()
-        except:
-            try:
-                scheme, netloc, path, params, query, fragment = urlparse(url)
-                url_service = '{}://{}/{}{}'.format(scheme, netloc, "service", path)
-                response = requests.get(url_service).json()
-            except:
-                response = {}
+        response = requests.get(url)
+        if response.status_code != 200:
+            return url
         
-        return response
+        return response.json()
 
     #Inbox object stores the id of the original item
     def get_items(self, inbox):
         #Get original item through their id
+        request = self.context.get('request')
         try:
             if inbox.like_object:
-                response = self.make_request(inbox.like_object.id)
+                req = Inbox.objects.get(like_object__id=inbox.like_object.id)
+                response = LikeSerializerGet(req.like_object, context={'request':request}).data
 
             elif inbox.post_object:
-                response = self.make_request(inbox.post_object.id)
+                req = Inbox.objects.get(post_object__id=inbox.post_object.id)
+                response = PostSerializerGet(req.post_object, context={'request':request}).data
 
             elif inbox.comment_object:
-                response = self.make_request(inbox.comment_object.id)
+                req = Inbox.objects.get(comment_object__id=inbox.comment_object.id)
+                response = CommentSerializerGet(req.comment_object, context={'request':request}).data
 
             elif inbox.follow_request_object:
                 follow_request = inbox.follow_request_object
