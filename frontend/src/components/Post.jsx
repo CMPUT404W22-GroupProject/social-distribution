eff09ad12e7d2f0dfbeaca0b8d6ae4e32d11250c
Gurjog Singh <gurjog@ualberta.ca>
2022-03-28 19:35:48 -0600

    tried user profile, but failed.

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -12,60 +12,47 @@ import {format} from "timeago.js"
 import ReactMarkdown from "react-markdown";
 import PopupSmall from './popupSmall/PopupSmall'
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
+import { PsychologyTwoTone } from '@mui/icons-material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useRef } from 'react';
 
-function Post({post}){
+function Post({post, team, loggedInAuthor}){
     //This is the main post card that handles post events, and houses the like, comment and share events too
     const [buttonPopup, setButtonPopup] = useState(false);
     const [like, setLike] = useState(0); //initial like value obtained from server and set in useEffect bellow
     const [likeObjects, setLikeObjects] = useState([]);
     const [isLiked, setIsLiked] = useState(false);
-    const postUrl = new URL(post.id); //this is full url that includes http://localhost:8000/ stuff
-    const postPath = postUrl.pathname; //this is path of post e.g. authors/{authorid}/posts/{postid}
     const hasImage = false;
     const postAuthor = post.author; //entire author object derived from post
     const postAuthorId = postAuthor.id; //this is just the ID of POST author, NOT entire object,
     const commentCount = post.count; //comment counter obtained from server
     //const myAuthorId = "53f89145-c0bb-4a01-a26a-5a3332e47156"; // this is my user author id, get from Context // JACK SPARROW
-    //const myAuthorIdUrl = "https://cmput-404-w22-group-10-backend.herokuapp.com/authors/53f89145-c0bb-4a01-a26a-5a3332e47156"; //this is my user author id path, ideally retrieved from Context// JACK SPARROW
-    const myAuthorId = "9170ef2f-501c-47c7-a8b2-99480fb49216" // MOE
-    const myAuthorIdUrl = "https://cmput-404-w22-group-10-backend.herokuapp.com/authors/9170ef2f-501c-47c7-a8b2-99480fb49216" //MOE
+    //const loggedInAuthor.id = "https://cmput-404-w22-group-10-backend.herokuapp.com/authors/53f89145-c0bb-4a01-a26a-5a3332e47156"; //this is my user author id path, ideally retrieved from Context// JACK SPARROW
+    //const myAuthorIdUrl = "https://cmput-404-w22-group-10-backend.herokuapp.com/authors/9170ef2f-501c-47c7-a8b2-99480fb49216" //MOE
     const [author, setAuthor] = useState({});
     const commentsSrc = post.commentsSrc;
     const [likeId, setLikeId] = useState(0);
     const [buttonSmallPopupForLike, setButtonSmallPopupForLike] = useState(false);
     const [buttonSmallPopupForShare, setButtonSmallPopupForShare] = useState(false);
+    const [buttonSmallPopupForEdit, setButtonSmallPopupForEdit] = useState(false);
     const team4Authorization = btoa("Team10:abcdefg");
     const team9Authorization = btoa("group10:pwd1010");
-    const team10Authorization = btoa("admin:gwbRqv8ZLtM3TFRW");;
+    const team10Authorization = btoa("admin:gwbRqv8ZLtM3TFRW");
     const postHostName = new URL(post.id).hostname;
+    const [isPlain, setisPlain] = useState(false);
+    const [isMarkdown, setIsMarkdown] = useState(false);
+    const [postTitle, setPostTitle] = useState(post.title);
+    const [postDescription, setPostDescription] = useState(post.description);
+    const [postContent, setPostContent] = useState(post.content);
+    const [postTags, setPostTags] = useState(post.categories);
+    const [isPublic, setIsPublic] = useState(false);
 
-    //console.log("POST DATA: ", post)
-    /* NOTES FOR MYSELF
-
-    right now author info is being fetched, not needed in the future as author info will come in post object
-
-
-    */
 
 
      useEffect(() => {
          //fetches data from the server
-       
-        const fetchAuthor = async () => {
-            const result = await axios.get("https://cmput-404-w22-group-10-backend.herokuapp.com/authors/" + myAuthorId + "/", {
-                headers: {
-                  'Authorization': 'Basic ' + team10Authorization
-                }
-              });
-            setAuthor(result.data);
-        } */
         const fetchLikeCount = async () => {
-            await axios.get(postPath + "/likes")
-            .then((response) => {
-                const result = response;
-                const likeObjectRecieved = response.data;
-                hasAuthorAlreadyLiked(likeObjectRecieved);
-
             if (postHostName === "cmput-404-w22-group-10-backend.herokuapp.com") {
                 try {
                         const postUrl = new URL(post.id); //this is full url that includes http://localhost:8000/ stuff
@@ -79,6 +66,7 @@ function Post({post}){
                         const result = response;
                         const likeObjectRecieved = response.data;
                         hasAuthorAlreadyLiked(likeObjectRecieved);
+                        
 
                         if (result.data.length !== undefined){
                             setLike(result.data.length);
@@ -110,10 +98,8 @@ function Post({post}){
                 } catch (error) {
                     //console.log(error)
                 }
-            });
-            console.log("POSTAUJTHORID: ", postAuthorId);
+            }
 
-            
             /* if (result.data.length !== undefined ){
                 if (result.data.some(i => i.id.includes(likeId))){
                     console.log("IT INCLUDES")
@@ -125,30 +111,56 @@ function Post({post}){
                 }
             } */
         }
-
         const hasAuthorAlreadyLiked = (likeObjectRecieved) => {
             likeObjectRecieved.forEach((like) => {
                 //chekcing to see if logged in user has already liked the post i.e. seeing if logged in user is already in like object list
                 // if so, then isLiked will be set to true. This is will avoid user liking the same object multiple times. 
-                console.log("like.author.id: ", like)
-                    console.log("myAuthorIdUrl: ", myAuthorIdUrl)
-                if (like.author.id === myAuthorIdUrl){
+                //console.log("like.author.id: ", like)
+                    //console.log("myAuthorIdUrl: ", myAuthorIdUrl)
+                if (like.author.id === loggedInAuthor.id){
                     //console.log("SET TO TRUE")
                     setIsLiked(true);
-                    const likeIdUrl = new URL(like.id);
-                    const likeIdUrlPath = likeIdUrl.pathname;
-                    setLikeId(likeIdUrlPath);
+                    //const likeIdUrl = new URL(like.id);
+                    //const likeIdUrlPath = likeIdUrl.pathname;
+                    //console.log("LIKE OBJECT: ", like)
+                    setLikeId(like.id);
                 }
             })
         } 
         fetchLikeCount();
-        //fetchAuthor();
+        if (post.visibility == "PUBLIC"){
+            setIsPublic(true);
+        }
     },[])
+
  
     const likeHandler = async () => {
         //handles how a like is sent, and manages likes/dislikes
         var newLike = {
-            "author": myAuthorId, //just sending in ID
+            "author": loggedInAuthor.id, //just sending in ID
+        }
+
+        /* {
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "summary": "Lara Croft Likes your post",         
+            "type": "Like",
+            "author":{
+                "type":"author",
+                "id":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                "host":"http://127.0.0.1:5454/",
+                "displayName":"Lara Croft",
+                "url":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                "github":"http://github.com/laracroft",
+                "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+            },
+            "object":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e"
+       } */
+        var remoteNewLike = {
+            "@context": "https://www.w3.org/ns/activitystreams",
+            "summary": loggedInAuthor.displayName + " likes your post",
+            "type" : "Like",
+            "author": loggedInAuthor,
+            "object": post.id
         }
        // console.log("REMOTENEWLIKE: ", remoteNewLike)
        
@@ -187,11 +199,11 @@ function Post({post}){
                     //console.log(error)
                 }
                 }  else {
-                    console.log("DELETED LIKE");
+                    //console.log("DELETED LIKE");
                     try {
                         //await axios.delete( postPath + "/likes/" + likeId)
                         //likeId is already full path
-                        console.log("LIKEID: ", likeId)
+                        //console.log("LIKEID: ", likeId)
                         await axios.delete(likeId, {
                             headers: {
                               'Authorization': 'Basic ' + team10Authorization
@@ -219,7 +231,7 @@ function Post({post}){
                         ////console.log(error)
                     }
                 } else {
-                    console.log("DELETED LIKE");
+                    //console.log("DELETED LIKE");
                     try {
                         //await axios.delete( postPath + "/likes/" + likeId)
                         //likeId is already full path
@@ -228,23 +240,24 @@ function Post({post}){
                         ////console.log(error)
                     }
                 }
-
             }
-        } 
 
         //if user has already liked it and called, will decrement. if user hasnt liked, will increment. 
         setLike(isLiked ? like - 1: like + 1); 
         //changes isliked state of user
         setIsLiked(!isLiked) 
 
-        console.log("like changed!: ", like, isLiked);
+        //console.log("like changed!: ", like, isLiked);
     }
 
-    const shareHandler = () => {
+    const shareHandler = async () => {
+       
+        console.log("ORIGINAL POST", post)
+        var sharedPost = post;
         
-        sharedPost["author"] = author;
+        sharedPost["author"] = loggedInAuthor;
         var origTitle = sharedPost["title"];
-        sharedPost["title"] = author.displayName + " shared " + post.author.displayName + "'s post: " + origTitle;
+        sharedPost["title"] = loggedInAuthor.displayName + " shared " + post.author.displayName + "'s post: " + origTitle;
         if (sharedPost["origin"] !== ""){
             sharedPost["source"] = post.id;
         } else {
@@ -258,7 +271,7 @@ function Post({post}){
         console.log("POST TO BE SHARED: ", sharedPost)
         
         try {
-            await axios.post(myAuthorIdUrl + "/posts/", sharedPost, {
+            await axios.post(loggedInAuthor.id + "/posts/", sharedPost, {
                 headers: {
                   'Authorization': 'Basic ' + team10Authorization
                 }
@@ -271,7 +284,7 @@ function Post({post}){
             });
 
             var followers;
-            await axios.get(myAuthorIdUrl + "/followers/", {
+            await axios.get(loggedInAuthor.id + "/followers/", {
                 headers: {
             'Authorization': 'Basic ' + team10Authorization
                 }
@@ -328,6 +341,55 @@ function Post({post}){
 
     }
 
+    const editHandler = () => {
+        console.log("TIME TO EDIT")
+
+        var editedPost = post;
+
+        editedPost["title"] = postTitle;
+        editedPost["description"] = postDescription;
+        editedPost["content"] = postContent;
+        editedPost["categories"] = postTags;
+        
+        console.log("EDITED POST: ", editedPost)
+
+        //Sending edited post to author's posts
+        axios.put(post.id + '/', editedPost, {
+            headers: {
+              'Authorization': 'Basic ' + team10Authorization
+            }
+          })
+          .then((response) => {
+            if (response.status === 201) {
+                alert("Updated! Refresh page to see edits!");
+                //window.location.href = window.location.href;
+                } else {
+                  alert("Oops! Something went wrong! Please try again!");
+                }
+          })
+
+    }
+
+    const deleteHandler = () => {
+
+        console.log("TIME TO DELETE")
+        
+        axios.delete(post.id,{
+            headers: {
+              'Authorization': 'Basic ' + team10Authorization
+            }
+        })
+        .then((response) => {
+            if (response.status === 204) {
+                alert("Deleted successfully!");
+                //window.location.href = window.location.href;
+                } else {
+                  alert("Oops! Something went wrong! Please try again!");
+                }
+          })
+
+    }
+
     return(
         <div className='postCard'>
             <Card >
@@ -342,6 +404,10 @@ function Post({post}){
                         <span className="postDate">
                             {format(post.published)}
                         </span>
+                       { (post.author.id === loggedInAuthor.id) && (isPublic) && <span className ="postEditDelete">
+                            <EditIcon htmlColor='purple' onClick = {()=>{setButtonSmallPopupForEdit(true)}}/>
+                            <DeleteIcon htmlColor='red' onClick = {() => {deleteHandler()}}/>
+                        </span>}
                      </div> 
                 </Card.Header>
                 <Card.Title className='postTitle'>
@@ -367,9 +433,6 @@ function Post({post}){
                         <ReactMarkdown children= {post.content} ></ReactMarkdown>
                     }
 
-
-                    
-                     
                 </Card.Body>
                 <Card.Subtitle className='postTags'>
                     Tags: {post.categories}
@@ -425,7 +488,7 @@ function Post({post}){
                         <CommentSection
                             team = {team}
                             //myAuthorId = {myAuthorId}
-                            myAuthor = {author}
+                            myAuthor = {loggedInAuthor}
                             commentsId = {post.comments}
                             commentCount = {commentCount}
                             postAuthorId = {postAuthor.id}/>
@@ -434,13 +497,13 @@ function Post({post}){
                         <CommentSection 
                             team = {team}
                             //myAuthorId = {myAuthorId}
-                            myAuthor = {author}
+                            myAuthor = {loggedInAuthor}
                             commentsId = {post.comments}
                             commentCount = {commentCount}
                             postAuthorId = {postAuthor.id}/>
                     }
 
-         
+                    
                 </Popup>
 
                 <PopupSmall trigger = {buttonSmallPopupForLike} setTrigger = {setButtonSmallPopupForLike}>
@@ -469,18 +532,44 @@ function Post({post}){
                    
                 </PopupSmall>
 
-                <PopupSmall trigger = {buttonSmallPopupForShare} setTrigger = {setButtonSmallPopupForShare}>
+                <PopupSmall style = {{height: "20px !important"}} trigger = {buttonSmallPopupForShare} setTrigger = {setButtonSmallPopupForShare}>
                     
                 
-                     <div>
+                     <div style = {{display: "flex"}}>
                      <span>
-                         SHARE
+                         Share this post?
                      </span>
+                     <Button onClick={()=>{shareHandler()}}>Share</Button>
                     </div>
                     
-                    
-              
-                   
+                </PopupSmall>
+
+                <PopupSmall trigger = {buttonSmallPopupForEdit} setTrigger = {setButtonSmallPopupForEdit}>
+                    <span>Edit Post</span>
+
+                    <label htmlFor="postTitle">Title:</label>
+                    <input name = "postTile"
+                        value = {postTitle}
+                        onChange = {(e) => {setPostTitle(e.target.value)}}
+                    />
+                    <label htmlFor="postDescription">Description:</label>
+                    <input name = "postDescription"
+                        value = {postDescription}
+                        onChange = {(e) => {setPostDescription(e.target.value)}}
+                    />
+                    <label htmlFor="postContent">Content:</label>
+                    <textarea name = "postContent"
+                        value = {postContent}
+                        onChange = {(e) => {setPostContent(e.target.value)}}
+                    />
+                    <label htmlFor="postTags">Tags:</label>
+                    <input name = "postTags"
+                        value = {postTags}
+                        onChange = {(e) => {setPostTags(e.target.value)}}
+                    />
+
+                    <Button onClick={()=>{editHandler()}}>Update</Button>
+
                 </PopupSmall>
         </div>
     )
