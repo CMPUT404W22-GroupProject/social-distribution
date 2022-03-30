5f2862d0c57d2fbf0331a49914721bdee0a5af28
moenuma <moenumasawa@gmail.com>
2022-03-30 18:56:40 +0900

    Add allow origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,13 +33,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:3000',
-]
-
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ]
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'https://bingbing10.herokuapp.com'
 ]
 
 # Application definition
