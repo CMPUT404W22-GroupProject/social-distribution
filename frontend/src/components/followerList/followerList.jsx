7bac778f877a10a43036dbbde081b7d1698e0c4d
tpbach <tpbach@ualberta.ca>
2022-04-01 17:19:11 -0600

    minor code cleanup

--- frontend/src/components/followerList/followerList.jsx
+++ frontend/src/components/followerList/followerList.jsx
@@ -7,9 +7,11 @@ import './followerList.css'
 function FollowerList({profileId}) {
 
   const [followers, setFollowers] = useState([])
-  const URL10 = "https://cmput-404-w22-group-10-backend.herokuapp.com"
-  const URL9 = "https://cmput-404-w22-project-group09.herokuapp.com/service"
+  const URL0 = "http://tik-tak-toe-cmput404.herokuapp.com/authors/"
   const URL4 = "http://backend-404.herokuapp.com"
+  const URL9 = "https://cmput-404-w22-project-group09.herokuapp.com/service"
+  const URL10 = "https://cmput-404-w22-group-10-backend.herokuapp.com"
+  const team0Authorization = btoa("admin:tX7^iS8a5Ky$^S");
   const team10Authorization = btoa("admin:gwbRqv8ZLtM3TFRW");
   const team9Authorization = btoa("group10:pwd1010");
   const team4Authorization = btoa("Team10:abcdefg");
