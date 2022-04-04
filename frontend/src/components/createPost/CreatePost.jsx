ecd2f25a420d541e8741bc1af53341b9261353f3
moenuma <moenumasawa@gmail.com>
2022-04-04 15:10:47 +0900

    fix createPost css

--- frontend/src/components/createPost/CreatePost.jsx
+++ frontend/src/components/createPost/CreatePost.jsx
@@ -1,8 +1,9 @@
 import './createPost.css'
-import {Button} from 'react-bootstrap'
+import {Button, FormCheck} from 'react-bootstrap'
 import {PermMedia} from '@mui/icons-material'
 import PersonIcon from '@mui/icons-material/Person'
 import PublicIcon from '@mui/icons-material/Public'
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import LockIcon from '@mui/icons-material/Lock'
 import PeopleIcon from '@mui/icons-material/People'
 import { useRef } from 'react'
@@ -10,6 +11,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import UserContext from '../../context/userContext';
 import PopupSmall from '../popupSmall/PopupSmall'
+import BootstrapSwitchButton from 'bootstrap-switch-button-react'
 
 function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers}){
     const postContent = useRef();
@@ -20,7 +22,7 @@ function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers})
     const [file, setFile] = useState(null); //used for storing the image that is uploaded
     const [base64, setBase64] = useState("");
     const [isPublic, setIsPublic] = useState(false);
-    const [isPrivate, setIsPrivate] = useState(false);
+    const [isPrivate, setIsPrivate] = useState(true);
     const [isFriend, setIsFriend] = useState(false);
     const [friend, setFriend] = useState({});
     const [isUnlisted, setIsUnlisted] = useState(false);
@@ -30,6 +32,7 @@ function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers})
     const [buttonPopup, setButtonPopup] = useState(false);
     const [isPlain, setisPlain] = useState(false);
     const [isMarkdown, setIsMarkdown] = useState(false);
+    // const [isImage, setIsImage] = useState(true);
     const team0Authorization = btoa("admin:tX7^iS8a5Ky$^S");
     const team4Authorization = btoa("Team10:abcdefg");
     const team9Authorization = btoa("group10:pwd1010");
@@ -285,54 +288,130 @@ function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers})
         })*/
 
     }
+
+
+    function contentTypeChange(event) {
+      event.preventDefault();
+      let contentTypeTemp = document.getElementById("contentTypeField").value
+      if (contentTypeTemp == "text/plain" || contentTypeTemp == "text/markdown") {
+        document.getElementById("contentField").style.display = "inline";
+        document.getElementById("imageField").hidden = true;
+      } else {
+        document.getElementById("contentField").style.display = "none";
+        document.getElementById("imageField").hidden = false;
+      }
+    }
+
+    function visibilityChange(checked) {
+      if (checked) {
+        setIsPublic(true);
+        setIsPrivate(false);
+        document.getElementById("chooseFriendField").hidden = true;
+      } else {
+        setIsPublic(false);
+        setIsPrivate(true);
+        document.getElementById("chooseFriendField").hidden = false;
+    }
+  }
+    function unlistedChange(checked) {
+      if (checked) {
+        setIsUnlisted(true);
+      } else {
+        setIsUnlisted(false);
+      }
+    }
     
     return (
         <div className="createPost">
           <div className="createPostWrapper">
             <div className="createPostTop">
+              <span class="createPostHead">Create a new post</span>
               {/* <img className="pofile-pic" src="/assets/person/1.jpeg" alt="" /> */}
-              {(loggedInAuthor.profileImage === "" || loggedInAuthor.profileImage === null) && 
+              {/* {(loggedInAuthor.profileImage === "" || loggedInAuthor.profileImage === null) && 
               <PersonIcon className='createPostProfilepic'/>}
               {(loggedInAuthor.profileImage !== "" && loggedInAuthor.profileImage !== null) && 
-              <img className='createPostProfilepic' src = {loggedInAuthor.profileImage}/>}
-              <input
-                placeholder="Title!"
-                className="createPostInput"
-                ref={postTitle}
-              />
-              <input
-                placeholder="Describe it a little"
-                className="createPostInput"
-                ref={postDescription}
-              />
-              <label htmlFor="contentType">Choose contentType:</label>
-              <select name = "contentType">
-                <option value = "text/plain" onClick = {() => setisPlain(true)}>text/plain</option>
-                <option value = "text/markdown" onClick = { () => setIsMarkdown(true)}>text/markdown</option>
+              <img className='createPostProfilepic' src = {loggedInAuthor.profileImage}/>} */}
+              <div class="form-group">
+                <label>Title</label>
+                <input placeholder="Give it a title!" className="form-control" ref={postTitle}/>
+              </div>
+              <div class="form-group">
+                <label>Description</label>
+                <input
+                  placeholder="What is this post about?"
+                  className="form-control"
+                  // className="createPostInput"
+                  ref={postDescription}
+                />
+              </div>
+              <div class="form-group">
+                <label htmlFor="contentType">Choose a content type</label>
+                <select class="form-control" name = "contentType" id="contentTypeField" onChange={contentTypeChange}>
+                  <option value = "text/plain" onClick = {() => setisPlain(true)}>text/plain</option>
+                  <option value = "text/markdown" onClick = { () => setIsMarkdown(true)}>text/markdown</option>
+                  <option value = "image">image</option>
                 </select>
+              </div>
               {/* <input
                 placeholder="What's in your mind?"
                 className="createPostInput"
                 ref={postContent}
               /> */}
-              <textarea
-                placeholder="What's in your mind?"
-                className="createPostInput"
-                ref={postContent}
-              />
-              <input
-                placeholder="Add tags!"
-                className="createPostInput"
-                ref={postTags}
-              />
+              <div class="form-group" id="contentField">
+                <label>Add content</label>
+                <textarea
+                  placeholder="What's in your mind?"
+                  className="form-control"
+                  rows="3"
+                  ref={postContent}
+                />
+              </div>
+              
+              <div class="form-group" id="imageField" hidden>
+                <label for="formFile" class="form-label">Upload image</label>
+                <input class="form-control" type="file" id="file" 
+                accept=".png, .jpeg" 
+                onChange={(e) => uploadImage(e)}
+                />
+              </div>
+              <div class="form-group">
+                <label>Tags</label>
+                <input
+                  placeholder="Add tags!"
+                  className="form-control"
+                  ref={postTags}
+                />
+              </div>
+              <div class="form-group">
+                <label>Visibility</label>
+                <div class="visibilityField">
+                  <BootstrapSwitchButton width={100} height={30} checked={false}
+                    onstyle="warning"
+                    onlabel='Public'
+                    offlabel='Private'
+                    onChange={(checked: boolean) => visibilityChange(checked)}
+                  />
+                  <div id = "chooseFriendField" className="chooseFriendButton" onClick={() => setButtonPopup(true)}>
+                      <PeopleAltIcon htmlColor="black" className="createPostIcon" />
+                      <span className="chooseFriendText">Choose friends to share</span>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"onChange={(checked: boolean) =>unlistedChange(checked)}/>
+                <label class="form-check-label" for="flexCheckDefault">
+                  Unlist
+                </label>
+              </div>
+              
             </div>
-
-            <hr className="createPostHr"/>
+{/* 
+            <hr className="createPostHr"/> */}
 
             <form className="createPostBottom" onSubmit={submitPost}>
-                <div className="createPostOptions">
+                {/* <div className="createPostOptions"> */}
 
-                    <label htmlFor = "file" className="createPostOption">
+                    {/* <label htmlFor = "file" className="createPostOption">
                         <PermMedia htmlColor="tomato" className="createPostIcon" />
                         <span className="createPostOptionText" >Photo</span>
                         <input 
@@ -343,9 +422,9 @@ function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers})
                         onChange={(e) => uploadImage(e)}
                         >
                         </input>
-                    </label>
+                    </label> */}
 
-                    <div className="createPostOption" onClick={choosePublic}>
+                    {/* <div className="createPostOption" onClick={choosePublic}>
                         <PublicIcon htmlColor="green" className="createPostIcon" />
                         <span className="createPostOptionText">Public?</span>
                     </div>
@@ -358,17 +437,17 @@ function CreatePost({loggedInAuthor, loggedInAuthorId, loggedInAuthorFollowers})
 
                     <div className="createPostOption" onClick={() => setButtonPopup(true)}>
                         <PeopleIcon htmlColor="blue" className="createPostIcon" />
-                        <span className="createPostOptionText">Choose friend</span>
-                    </div>
+                        <span className="createPostOptionText">Choose friend</span> */}
+                    {/* </div> */}
 
-                    <div className="createPostOption" onClick={chooseUnlisted}>
+                    {/* <div className="createPostOption" onClick={chooseUnlisted}>
                         <PeopleIcon htmlColor="blue" className="createPostIcon" />
                         <span className="createPostOptionText">Unlisted?</span>
-                    </div>
+                    </div> */}
 
-                </div>
+                {/* </div> */}
 
-                <Button className="createPostButton" type = "submit"  >Share</Button>
+                <Button className="createPostButton" type = "submit"  >Post</Button>
         
             </form>
           </div>
